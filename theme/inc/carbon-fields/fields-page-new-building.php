0dcae2eee045a1427e7afb034383ffff86f35c04
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-09-15 21:20:04 +0300

    feat: add fields new-building

--- theme/inc/carbon-fields/fields-page-new-building.php
+++ theme/inc/carbon-fields/fields-page-new-building.php
@@ -16,5 +16,17 @@ function page_new_building_fields()
     ->add_tab('Главная', array(
       Field::make('text', 'crb_new_building_title', 'Заголовок')->set_help_text('Город указывать не нужно'),
 
-    ));
+    ))->add_tab('Жилые комплексы', [
+      Field::make('complex', 'crb_gk', 'Жилые комплексы')
+        ->add_fields([
+          Field::make('text', 'crb_gk_name', 'Заголовок')->set_width(100),
+          Field::make('image', 'crb_gk_plan', 'План зайстройки')->set_width(25),
+          Field::make('media_gallery', 'crb_gk_gallery', 'Галерея')->set_width(75),
+          Field::make('textarea', 'crb_gk_description', 'Описание')->set_width(100),
+          Field::make('text', 'crb_gk_address', 'Адрес')->set_help_text('пр-кт Дзержинского')->set_width(100),
+          Field::make('text', 'crb_gk_latitude', 'Ширина')->set_help_text('44.75047100002018')->set_width(50),
+          Field::make('text', 'crb_gk_longitude', 'Долгота')->set_help_text('37.730149')->set_width(50),
+        ])
+        ->set_max(3)
+    ]);
 }

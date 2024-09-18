c6eac8672454dd6141d3c10321f9f85320912790
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-09-18 22:56:41 +0300

    refactor: add value in page_gk

--- theme/template-page/components/card_gk_single.php
+++ theme/template-page/components/card_gk_single.php
@@ -1,1 +1,140 @@
 <?php
+
+$crb_gk_name = $args['crb_gk_name'];
+$crb_gk_plan = $args['crb_gk_plan'];
+$crb_gk_gallery = $args['crb_gk_gallery'];
+$crb_gk_description =  $args['crb_gk_description'];
+$crb_gk_city =  $args['crb_gk_city'];
+$crb_gk_address = $args['crb_gk_address'];
+$crb_gk_latitude =  $args['crb_gk_latitude'];
+$crb_gk_longitude = $args['crb_gk_longitude'];
+$update_page = $args['update_page'];
+$min_price = $args['min_price'];
+$min_price_meter = $args['min_price_meter'];
+$finishing = $args['finishing'];
+
+$image_preview_url = '';
+$image_preview_url_two = '';
+$image_preview_url_three = '';
+
+if (!empty($crb_gk_gallery[0])) {
+    $image_preview_url  = wp_get_attachment_image_src($crb_gk_gallery[0], 'full');
+}
+if (!empty($crb_gk_gallery[1])) {
+    $image_preview_url_two  = wp_get_attachment_image_src($crb_gk_gallery[1], 'full');
+}
+if (!empty($crb_gk_gallery[2])) {
+    $image_preview_url_three  = wp_get_attachment_image_src($crb_gk_gallery[2], 'full');
+}
+
+$all_finishing = implode(', ', $finishing);
+?>
+
+<section class="single-gk-card">
+    <div class="single-gk-card__container">
+        <div class="single-gk-card-wrapper">
+            <div class="single-gk-card__info">
+                <div class="single-gk-card__title">
+                    <button class="button-back" type="button" aria-label="Назад"></button>
+                    <h1 class="title--xl title--catalog title--singe-page"><?php echo $crb_gk_name ?></h1>
+                </div>
+                <p class="single-gk-card__subtitle">
+                    <?php echo $crb_gk_city ?>, <?php echo $crb_gk_address ?>
+                    <a href="#single-map">Показать на карте</a>
+                </p>
+                <div class="product-wrapper">
+                    <div class="single-gk-card__product product">
+                        <div class="product__image-wrapper">
+                            <div class="product__image" data-type="popup-gallery">
+                                <div class="product-image-wrapper" data-type="popup-gallery" >
+                                    <img class="product-image-wrapper__preview" src="<?php echo $image_preview_url[0] ?>" alt="" data-type="popup-gallery" />
+                                </div>
+                            </div>
+                            <div class="product-single-slider swiper">
+                                <div class="product-single-slider__wrapper swiper-wrapper">
+                                    <?php if (!empty($crb_gk_gallery)) {
+                                        foreach ($crb_gk_gallery as $image) {
+                                            $image_url = wp_get_attachment_image_src($image, 'full');
+
+                                    ?>
+                                            <div class="product-single-slider__slide swiper-slide" data-type="popup-gallery" >
+                                                <img class="swiper-lazy" data-src="<?php echo $image_url[0] ?>" src="<?php bloginfo('template_url'); ?>'/assets/images/1px.png" alt="" data-type="popup-gallery" />
+                                                <div class="swiper-lazy-preloader"></div>
+                                            </div>
+
+                                    <? }
+                                    } ?>
+                                </div>
+                            </div>
+                            <div class="custom-scrollbar"></div>
+                            <div class="product__gallery">
+                                <div data-type="popup-gallery">
+                                    <img src="<?php echo $image_preview_url_two[0] ?>" alt="" width="226" height="166" />
+                                </div>
+                                <div data-type="popup-gallery">
+                                    <img src="<?php echo $image_preview_url_three[0] ?>" alt="" width="226" height="166" />
+                                    <span data-type="popup-gallery"><?php echo count($crb_gk_gallery) ?></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-single-slide-gallery swiper">
+                            <div class="product-single-slide-gallery__wrapper swiper-wrapper">
+                                <?php if (!empty($crb_gk_gallery)) {
+                                    foreach ($crb_gk_gallery as $image) {
+                                        $image_url = wp_get_attachment_image_src($image, 'full');
+
+                                ?>
+                                        <div class="product-single-slide-gallery__slide swiper-slide">
+                                            <img src="<?php echo $image_url[0] ?>" alt="" />
+                                        </div>
+                                <? }
+                                } ?>
+
+                            </div>
+                        </div>
+                        <div class="product__description">
+                            <div class="second-product-description" data-more-text>
+                                <?php echo $crb_gk_description ?>
+                            </div>
+                        </div>
+                        <div class="product__button-more">
+                            <button class="show-more" type="button" data-more><span>Узнать больше</span></button>
+                        </div>
+                    </div>
+                    <div class="single-gk-card__order">
+                        <article class="agent-order" data-agent-order>
+                            <p class="agent-order__date">Информация обновлена <?php echo $update_page ?></p>
+                            <h2 class="agent-order__price title--xl title--product-agent">от <?php echo number_format(round(floatval($min_price)), 0, '.', ' ') ?> ₽</h2>
+                            <div class="agent-order__label">Хорошая цена!</div>
+
+                            <div class="agent-order__info">
+                                <div class="agent-order__price-one-metr agent-price-one-mert">
+                                    <span class="agent-conditions__title">Отделка</span>
+                                    <span class="agent-conditions__space"></span>
+                                    <span class="agent-conditions__price"><?php echo $all_finishing ?></span>
+                                </div>
+                                <div class="agent-order__price-one-metr agent-price-one-mert">
+                                    <span class="agent-conditions__title">Цена за метр</span>
+                                    <span class="agent-conditions__space"></span>
+                                    <span class="agent-conditions__price">от <?php echo number_format(round(floatval($min_price_meter)), 0, '.', ' ') ?> ₽/м² </span>
+                                </div>
+                            </div>
+                            <div class="button-wrapper">
+                                <div class="agent-order__button">
+                                    <a class="button button--phone-order" href="tel:+79104898888"><span> +7 910 489-88-...</span></a>
+                                    <button class="button--favorites-mobile" type="button" data-favorite-cookies="'64" data-button-favorite-mobile data-delete-favorite="1"><span></span></button>
+                                </div>
+                                <div class="agent-order__callback">
+                                    <button class="button button--callback" type="button" data-type="popup-form-callback"><span data-type="popup-form-callback">Перезвоните мне</span></button>
+                                </div>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+                <!-- <div class="product__gk-filter">@@include('../../components/page-gk/page-gk-filter.html')</div>
+                <div class="product__more">@@include('../../components/page-gk/page-gk-table.html')</div> -->
+            </div>
+        </div>
+    </div>
+</section>
--- theme/template-page/new-building-page-gk.php
+++ theme/template-page/new-building-page-gk.php
@@ -4,6 +4,59 @@ Template Name: Страница жилого комплекса
 */
 
 require_once get_template_directory() . '/inc/enums/default_enum.php';
+require_once get_template_directory() . '/inc/lib/get_slug_page.php';
+
+$id_page = get_the_ID();
+
+$slug_page = get_slug_page();
+$id_category_gk = get_term_by('slug', $slug_page, 'category')->term_id;
+
+$args_gk = array(
+  'post_type' => 'post', // Тип поста (может быть 'post', 'page', 'custom-post-type' и т.д.)
+  'posts_per_page' => -1, // Количество постов на странице (-1 для вывода всех постов)
+  'category__and' => [$id_category_gk],
+);
+
+$query = new WP_Query($args_gk);
+$total_posts = $query->found_posts;
+
+$price_all = [];
+$price_meter_all = [];
+$finishing = [];
+if ($query->have_posts()) {
+
+  while ($query->have_posts()) {
+    $query->the_post(); // Устанавливаем данные поста
+    $ip_post = get_the_ID();
+    $price = carbon_get_post_meta($ip_post, 'product-price');
+    $price_meter = carbon_get_post_meta($ip_post, 'product-price-meter');
+    $apartament_finishing = carbon_get_post_meta($ip_post, 'product-finishing');
+
+    $price_all[] = $price;
+    $price_meter_all[] = $price_meter;
+
+    if (!in_array($apartament_finishing, $finishing)) {
+      $finishing[] = $apartament_finishing;
+    }
+  }
+
+  wp_reset_postdata();
+}
+
+$params_page_gk = [
+  'crb_gk_name' => carbon_get_post_meta($id_page, 'crb_gk_name'),
+  'crb_gk_plan' => carbon_get_post_meta($id_page, 'crb_gk_plan'),
+  'crb_gk_gallery' => carbon_get_post_meta($id_page, 'crb_gk_gallery'),
+  'crb_gk_description' =>  carbon_get_post_meta($id_page, 'crb_gk_description'),
+  'crb_gk_city' =>  carbon_get_post_meta($id_page, 'crb_gk_city'),
+  'crb_gk_address' => carbon_get_post_meta($id_page, 'crb_gk_address'),
+  'crb_gk_latitude' =>  carbon_get_post_meta($id_page, 'crb_gk_latitude'),
+  'crb_gk_longitude' =>  carbon_get_post_meta($id_page, 'crb_gk_longitude'),
+  'update_page' => get_the_modified_date('d-m-Y', $page_id),
+  'min_price' => min($price_all),
+  'min_price_meter' => min($price_meter_all),
+  'finishing' => $finishing,
+];
 
 get_header();
 ?>
@@ -11,32 +64,33 @@ get_header();
   <div class="main-favorites">
     <?php $crb_new_building_title = carbon_get_post_meta(get_the_ID(), 'crb_new_building_title');
 
+
     $filter_city = explode('/', $_SERVER['REQUEST_URI'])[2];
     $countSale = 6;
 
     $title_city = $filter_city === 'novorossiysk' ? 'в Новороссийске' : ($filter_city === 'krasnodar' ? 'в Краснодаре' : '');
     $search_param_city = $filter_city === 'novorossiysk' ? 'Новороссийск' : ($filter_city === 'krasnodar' ? 'Краснодар' : '');
 
-    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+    // $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 
-    $args = array(
-      'post_type' => 'post', // Тип поста (может быть 'post', 'page', 'custom-post-type' и т.д.)
-      'posts_per_page' => 4, // Количество постов на странице (-1 для вывода всех постов)
-      'paged' => $paged
-    );
 
-    if (isset($search_param_city)) {
-      $args['meta_query'] = array(
-        array(
-          'key' => 'product-city',
-          'value' => $search_param_city,
-          'compare' => 'LIKE'
-        )
-      );
-    }
+    // $args = array(
+    //   'post_type' => 'post', // Тип поста (может быть 'post', 'page', 'custom-post-type' и т.д.)
+    //   'posts_per_page' => 4, // Количество постов на странице (-1 для вывода всех постов)
+    //   'paged' => $paged
+    // );
+    // if (isset($search_param_city)) {
+    //   $args['meta_query'] = array(
+    //     array(
+    //       'key' => 'product-city',
+    //       'value' => $search_param_city,
+    //       'compare' => 'LIKE'
+    //     )
+    //   );
+    // }
 
-    $query = new WP_Query($args);
-    $total_posts = $query->found_posts;
+    // $query = new WP_Query($args);
+    // $total_posts = $query->found_posts;
 
     // $desiredValue = $parts[2];
     if (function_exists('yoast_breadcrumb')) {
@@ -85,7 +139,7 @@ get_header();
               <h1 class="favorites__title title--xl title--catalog">
                 <?php echo $crb_new_building_title . ' ' . $title_city; ?>
               </h1>
-              <p class="favorites__subtitle">Найдено <?php echo num_word($total_posts, DEFAULT_ENUM::RESIDENTAL_COMPLEX) ?></p>
+              <!-- <p class="favorites__subtitle">Найдено <?php echo num_word($total_posts, DEFAULT_ENUM::RESIDENTAL_COMPLEX) ?></p> -->
             </div>
           </div>
           <div id='content-container-new-buildings' class="favorites-wrapper">
@@ -114,49 +168,49 @@ get_header();
               $postIds = [];
             }
 
+            get_template_part('template-page/components/card_gk_single', null, $params_page_gk);
+            // if ($query->have_posts()) {
+            //   $postCount = 0;
+            //   while ($query->have_posts()) {
+            //     $query->the_post();
 
-            if ($query->have_posts()) {
-              $postCount = 0;
-              while ($query->have_posts()) {
-                $query->the_post();
+            //     $params = [];
 
-                $params = [];
+            //     get_template_part('template-page/components/card_gk_single', null, $params);
+            //   }
+            //   if ($query->max_num_pages > 1) {
+            //     echo '<div class="pagination">
+            //                 <div class="pagination__container">
+            //                  ';
+            //     $prev_link = get_previous_posts_link('Назад');
+            //     if ($prev_link) {
+            //       echo '<span class="prev-link">' . $prev_link . '</span>';
+            //     } else {
+            //       echo '<span class="prev-link disabled">Назад</span>';
+            //     }
 
-                get_template_part('template-page/components/card_gk_single', null, $params);
-              }
-              if ($query->max_num_pages > 1) {
-                echo '<div class="pagination">
-                            <div class="pagination__container">
-                             ';
-                $prev_link = get_previous_posts_link('Назад');
-                if ($prev_link) {
-                  echo '<span class="prev-link">' . $prev_link . '</span>';
-                } else {
-                  echo '<span class="prev-link disabled">Назад</span>';
-                }
+            //     echo paginate_links(array(
+            //       'total' => $query->max_num_pages,
+            //       'current' => $paged,
+            //       'prev_next' => false,
+            //       'end_size' => 1,
+            //       'mid_size' => 2,
+            //       'type' => 'list',
+            //     ));
 
-                echo paginate_links(array(
-                  'total' => $query->max_num_pages,
-                  'current' => $paged,
-                  'prev_next' => false,
-                  'end_size' => 1,
-                  'mid_size' => 2,
-                  'type' => 'list',
-                ));
-
-                $next_link = get_next_posts_link('Дальше', $query->max_num_pages);
-                if ($next_link) {
-                  echo '<span class="next-link">' . $next_link . '</span>';
-                } else {
-                  echo '<span class="next-link disabled">Дальше</span>';
-                }
-                echo '
-                         </div>
-                        </div>';
-              }
-            } else {
-              echo 'Посты не найдены.';
-            }
+            //     $next_link = get_next_posts_link('Дальше', $query->max_num_pages);
+            //     if ($next_link) {
+            //       echo '<span class="next-link">' . $next_link . '</span>';
+            //     } else {
+            //       echo '<span class="next-link disabled">Дальше</span>';
+            //     }
+            //     echo '
+            //              </div>
+            //             </div>';
+            //   }
+            // } else {
+            //   echo 'Посты не найдены.';
+            // }
 
             wp_reset_postdata();
             ?>
@@ -198,6 +252,9 @@ get_header();
     </div>
   </div>
 </main>
+<div class="popup-gallery">
+  <?php get_template_part('template-page/popup/popup-gallery-gk') ?>
+</div>
 <?php
 get_footer();
 ?>

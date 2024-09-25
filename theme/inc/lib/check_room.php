40d85a899738714dfc79ce0a8eb89e0898ef2217
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-09-25 21:28:50 +0300

    fix: check_room

--- theme/inc/lib/check_room.php
+++ theme/inc/lib/check_room.php
@@ -6,7 +6,7 @@ function check_room($id_room)
 {
 
     $appartaments = [1, 22, 1001, 2, 0, 60, 3, 4, 6, 23, 25, 35, 5, 24, 7];
-    $house = [30];
+    $house = [30, 40];
 
     if (in_array($id_room, $appartaments)) {
         return DEFAULT_ENUM::APARTAMENTS;

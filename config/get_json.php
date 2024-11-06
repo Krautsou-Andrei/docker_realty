e67350336ff5af87fc15ee9de7f8a23a097d548d
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-11-06 22:34:33 +0300

    DEPLOY: change docker composer

---
+++ config/get_json.php
@@ -0,0 +1,9 @@
+<?php
+require_once('wp-load.php');
+// require_once get_template_directory() . '/get_json.php';
+require_once get_template_directory() . '/apatraments-json.php';
+// require_once get_template_directory() . '/inc/lib/delete_old_posts.php';
+
+// my_custom_task();
+start();
+// delete_old_posts();

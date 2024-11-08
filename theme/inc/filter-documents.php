190afccd40ac968d402e1b074443c72f85594c1c
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-11-08 23:19:17 +0300

    fix: filter_documents

--- theme/inc/filter-documents.php
+++ theme/inc/filter-documents.php
@@ -1,4 +1,5 @@
 <?php
+require_once get_template_directory() . '/inc/lib/get_image_url.php';
 if (!defined("ABSPATH")) {
     exit;
 }
@@ -10,28 +11,27 @@ function filter_documents_handler()
     $filter_type = $_POST['filterType'];
     $employeeId = $_POST['employee'];
     $min_show_documents = $_POST['limit'];
-    $page = $_POST['page'];;
-    
-    
-    // $is_show_button_next_page = false;
+    $page = $_POST['page'];
+
+
+
     $all_documents = [];
     $filter_documents = [];
-    
+
     $crb_employees_field = carbon_get_post_meta(5, 'crb_employees_field');
-    
-    
-    if($employeeId){
+
+
+    if ($employeeId) {
         $employee = $crb_employees_field[$employeeId];
         $all_documents =  $employee['crb_documents'];
     } else {
-        foreach($crb_employees_field as $employee){
+        foreach ($crb_employees_field as $employee) {
             array_push($all_documents, ...$employee['crb_documents']);
         };
-        
     }
-    
-    
-    
+
+
+
     ob_start();
 
     $documents_markup = '';
@@ -41,97 +41,76 @@ function filter_documents_handler()
         $type = $type_documents["_type"];
         $documents = $type_documents["crb_{$type}_gallery"];
 
-        if($filter_type === $type){
-            array_push($filter_documents, ...$documents);   
-        } 
-        
-        if($filter_type === 'all'){
-            array_push($filter_documents, ...$documents); 
-            
-            // if(count($filter_documents) > $min_show_documents){
-            //     $is_show_button_next_page = true;
-                
-            // }
-        } 
+        if ($filter_type === $type) {
+            array_push($filter_documents, ...$documents);
+        }
+
+        if ($filter_type === 'all') {
+            array_push($filter_documents, ...$documents);
+        }
+
+        $filter_documents = array_values($filter_documents);
     }
-    
-    if($search_type == 'employee'){
+
+    if ($search_type == 'employee') {
         foreach ($filter_documents as $document_id) {
-            $document_url = wp_get_attachment_image_src($document_id, 'full');
+            $document_url = wp_get_attachment_image_src($document_id, 'thumbnail');
             $document_alt = get_post_meta($document_id, '_wp_attachment_image_alt', true);
-    
-             $documents_markup .= '
+
+            $documents_markup .= '
                                   <li class="slider-documents-gallery-popup__slide swiper-slide document" >
                                       <figure>
                                         <div class="image-wrapper">
-                                          <img src="' . $document_url[0] . '"  alt="' . $document_alt . '" class="document__image" />
+                                          <img src="' . get_image_url($document_url)  . '"  alt="' . $document_alt . '" class="document__image" />
                                         </div>
                                         <figcaption>' . $document_alt . '</figcaption>
                                       </figure>
                                     </li>
                                   ';
         }
-        
-    } 
-    
-    if($search_type == "documents"){
+    }
+
+    if ($search_type == "documents") {
         foreach ($filter_documents as $index => $document_id) {
 
-            $document_url = wp_get_attachment_image_src($document_id, 'full');
-            $document_alt = get_post_meta($document_id, '_wp_attachment_image_alt', true);
-            
-            if($index < $min_show_documents * $page || $page === 'all'){
-        
-                $documents_markup .= '  <li class="slider-documents-gallery__slide swiper-slide document" data-type="popup-gallery-documents">
+            $document_url = wp_get_attachment_image_src($document_id, 'thumbnail');
+            $document_alt = '';
+
+            $documents_markup .= '  <li class="slider-documents-gallery__slide swiper-slide document" data-type="popup-gallery-documents">
                                           <figure data-type="popup-gallery-documents">
                                                 <div class="image-wrapper" data-type="popup-gallery-documents">
-                                                    <img src="' . $document_url[0] . '" alt="' . $document_alt . '" class="document__image" data-index-slider="'.$index.'" data-type="popup-gallery-documents" data-button-open-gallery-documents/>
-                                                
+                                                    <img src="' . get_image_url($document_url) . '" alt="' . $document_alt . '" class="document__image" data-index-slider="' . $index . '" data-type="popup-gallery-documents" data-button-open-gallery-documents/>
                                                 </div>
-                                                <figcaption data-index-slider="'.$index.'" data-type="popup-gallery-documents" data-button-open-gallery-documents>' . $document_alt . '</figcaption>
+                                                <figcaption data-index-slider="' . $index . '" data-type="popup-gallery-documents" data-button-open-gallery-documents>' . $document_alt . '</figcaption>
                                           </figure>
                                         </li>';
-            }
         };
-         
-        // if(count($filter_documents) > $min_show_documents * $page ){
-        //     $page ++;
-               
-        // }
-        
     }
-    
+
     $documents_gallery_markup = "";
-    
-    if($search_type == "documents-popup"){
+
+    if ($search_type == "documents-popup") {
         foreach ($filter_documents as $index => $document_id) {
 
-            $document_url = wp_get_attachment_image_src($document_id, 'full');
+            $document_url = wp_get_attachment_image_src($document_id, 'thumbnail');
             $document_alt = get_post_meta($document_id, '_wp_attachment_image_alt', true);
-            
-            
-        
-                $documents_markup .= '      <li class="slider-documents-preview__slide swiper-slide" data-swiper-slide-index="'.$index.'" role="group" aria-label="'.($index + 1).' / '.count($filter_documents).'">
+
+
+
+            $documents_markup .= '      <li class="slider-documents-preview__slide swiper-slide" data-swiper-slide-index="' . $index . '" role="group" aria-label="' . ($index + 1) . ' / ' . count($filter_documents) . '">
                                                 <div class="slide-wrapper">
-                                                  <img src="' . $document_url[0] . '" alt="' . $document_alt . '" />
+                                                  <img src="' . get_image_url($document_url)  . '" alt="' . $document_alt . '" />
                                                 </div>
                                             </li>';
-                                                        
-                $documents_gallery_markup .= '  <li class="slider-popup-documents-gallery__slide swiper-slide" role="group" aria-label="'.($index + 1).' / '.count($filter_documents).'">
-                                                 <img src="' . $document_url[0] . '" alt="' . $document_alt . ' width="82" height="64" />
-                                                </li>';                                        
-            
+
+            $documents_gallery_markup .= '  <li class="slider-popup-documents-gallery__slide swiper-slide" role="group" aria-label="' . ($index + 1) . ' / ' . count($filter_documents) . '">
+                                                 <img src="' . get_image_url($document_url)  . '" alt="' . $document_alt . ' width="82" height="64" />
+                                                </li>';
         };
-         
-        // if(count($filter_documents) > $min_show_documents * $page ){
-        //     $page ++;
-               
-        // }
-        
     }
-    
-    if($documents_markup === ""){
-            $documents_markup = '<li class="slider-documents-gallery-popup__slide swiper-slide document" style="width: 100%; display: flex; aling-items: center; justify-content: center">
+
+    if ($documents_markup === "") {
+        $documents_markup = '<li class="slider-documents-gallery-popup__slide swiper-slide document" style="width: 100%; display: flex; aling-items: center; justify-content: center">
                                   <figure>
                                     <div class="image-wrapper">
                                      <span>Данного вида документов не найдено</span>
@@ -139,19 +118,14 @@ function filter_documents_handler()
                                     <figcaption></figcaption>
                                   </figure>
                                 </li>';
-        }
-    
+    }
+
     $response = array(
-        // 'type' => $search_type,
-        // 'isShowButton' => $is_show_button_next_page,
-        // 'page' => $page,
         'documents' => $documents_markup,
         'documentsGallery' => $documents_gallery_markup,
     );
 
-    // wp_reset_postdata();
 
-    // $response = ob_get_clean();
 
     wp_send_json_success($response);
     wp_die();

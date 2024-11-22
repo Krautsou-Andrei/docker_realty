3d18121478c84d991f9444431c2a47a6a38e5923
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-11-22 09:03:37 +0300

    feat: delete empty latest post

--- theme/inc/lib/get_latest_post.php
+++ theme/inc/lib/get_latest_post.php
@@ -10,12 +10,18 @@ function get_latest_post()
 
     $latest_posts = get_posts($args);
 
-    $product_id = '';
+    $product_id = null;
 
     if (!empty($latest_posts)) {
         $latest_post = $latest_posts[0];
         $post_id = $latest_post->ID;
         $product_id = carbon_get_post_meta($post_id, 'product-id');
+
+        if ($product_id === '') {
+            wp_delete_post($post_id, true);
+            get_latest_post();
+        }
     }
+    
     return $product_id;
 }

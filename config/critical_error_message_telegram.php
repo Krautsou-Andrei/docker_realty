eb67b79be5e0da0a52112dad4d068fdc5bcdc159
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-11-25 12:55:23 +0300

    fix: cron

--- config/critical_error_message_telegram.php
+++ config/critical_error_message_telegram.php
@@ -2,6 +2,7 @@
 require_once('/var/www/html/wp-load.php');
 require_once get_template_directory() . '/apatraments-json.php';
 require_once get_template_directory() . '/inc/lib/get_message_server_telegram.php';
-
-start(true);
+$delay = 30;
 get_message_server_telegram("Критическая ошибка", "Скрипт отановлен");
+sleep($delay);
+start(true);
